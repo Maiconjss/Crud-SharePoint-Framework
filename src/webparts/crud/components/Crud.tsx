782f3820a99927b497e9943fb65be47da291202c
Maicon Ferreira <maicon.ferreira@trentim.com>
2020-12-28 03:04:19 -0300

    changes

--- src/webparts/crud/components/Crud.tsx
+++ src/webparts/crud/components/Crud.tsx
@@ -7,18 +7,7 @@ export default class Crud extends React.Component<ICrudProps, {}> {
   public render(): React.ReactElement<ICrudProps> {
     return (
       <div className={ styles.crud }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+              <h1>maicon</h1>
       </div>
     );
   }
